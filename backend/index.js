b060640ae09f6a1db6e567c1230e4b4dcb0dfe77
oln <victorprg0111@gmail.com>
2025-04-21 12:37:10 -0300

    remove cors

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,13 @@ const productsRoute = require('./routes/products')
 const port = process.env.PORT || 5000
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-const cors = require('cors')
+// const cors = require('cors')
 const authRoutes = require('./routes/auth')
 const path = require('path')
 
 dotenv.config()
 
-app.use(cors())
+// app.use(cors())
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('🟢 Connected to MongoDB Atlas'))
